05bca004187902315dc7ed25f54e6e2fda720f63
suicidepills <atom.mac@gmail.com>
2016-05-25 19:07:57 -0400

    Base what.cd save location on user input instead of guessing (for now)

--- torrent-sort.py
+++ torrent-sort.py
@@ -27,17 +27,22 @@ else:
 	# TODO: Attempt to launch Transmission
 
 # Read a torrent file name.
-file_name = sys.argv[1]
+try:
+	file_name = sys.argv[1]
 
-if file_name != None:
-	print 'Attempting to add torrent file: ', file_name
-else:
+	# Check to see if file exists
+	if not os.path.isfile(file_name):
+		print 'File name was given but doesn\'t exist :('
+		exit()
+	else:
+		print 'Attempting to add torrent file: ', file_name
+except IndexError as exc:
 	print 'I need a torrent file in order to work :('
 	sys.exit()
 
 # Connect w/ Transmission and add the torrent file to it.
 c = transmissionrpc.Client()
-t = c.add_torrent('file://' + file_name)
+t = c.add_torrent('file://' + file_name, paused=True)
 
 # Update torrent to ensure that all fields are populated
 t.update()
@@ -47,18 +52,33 @@ t.update()
 # Perform actions on Torrents based on critera read from Torrent object
 # Ex: For torrents from What.cd, move the torrent download location to the appropriate place in the Music directory structure.
 def handle_whatcd(torrent):
+	print 'Torrent from What.cd detected!'
+
 	# Settings for this action
 	DOWNLOAD_DIR = 'F:/Music'
 
-	# Gather data from the torrent
-	tmp = torrent.name.split(' - ')
+	# Gather torrent data from the user so we can figure out where to save the
+	# torrent's payload.
+	artist = ''
+	album  = ''
+	while len(artist) == 0:
+		artist = raw_input('Artist: ')
+		artist = artist.strip()
 
-	artist = tmp[0]
-	album  = tmp[1].split(' (')[0]
-	folder = artist[0].upper()
+	while len(album) == 0:
+		album = raw_input('Album : ')
+		album = album.strip()
+
+	# Determine what letter the artist name starts with, removing "the" from the
+	# beginning of all artist names (i.e. The Beatles)
+	the_pattern = re.compile('^The\s')
+	folder = the_pattern.sub('', artist)[0].upper()
 
 	# Make alphabet folder if not currently available
 	path = DOWNLOAD_DIR + '/' + folder + '/' + artist + '/' + album
+	
+	# Keep the user appraised of our progress
+	print 'Saving music to ', path
 
 	try:
 		os.mkdir(path)
@@ -67,6 +87,9 @@ def handle_whatcd(torrent):
 
 	# Move torrent data to the new path
 	c.move_torrent_data(torrent.id, path)
+
+	# Now that we know where the torrent will live, start downloading it!
+	c.start_torrent(torrent.id)
 
 # List of patterns to search for in tracker URLs and corresponding functions to
 # fire if a match is found
